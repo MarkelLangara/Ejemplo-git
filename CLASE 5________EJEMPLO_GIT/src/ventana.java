2071ed1507ace7b01678d2bbc6ef14d41b659f85
MarkelLangara <markel.langara@opendeusto.es>
2026-09-23 09:39:59 +0200

    Change window title from 'Prueba de Git' to 'AlexSantsPro'

--- CLASE 5________EJEMPLO_GIT/src/ventana.java
+++ CLASE 5________EJEMPLO_GIT/src/ventana.java
@@ -11,7 +11,7 @@ public class ventana extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(320, 240);
 		
-		setTitle("Prueba de Git");
+		setTitle("AlexSantsPro");
 		
 		
 		setVisible(true);
